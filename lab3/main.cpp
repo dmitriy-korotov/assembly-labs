f70029d16f3efea6c7fbaa75ea7af005b4040be2
reosfire <2.com9761721@gmail.com>
2024-03-08 14:53:58 +0300

    run script

--- lab3/main.cpp
+++ lab3/main.cpp
@@ -2,6 +2,41 @@
 #include <bitset>
 #include <climits>
 
-int main(int _argc, char* _argv[]) {
-    
+void print16(void* _ptr) noexcept
+{
+    unsigned short* p = reinterpret_cast<unsigned short*>(_ptr);
+    std::printf("h: %04X ", *p);
+    std::cout << "b: " << std::bitset<16>(*p) << " ";
+    std::printf("u: %05u ", *p);
+
+    short* p2 = reinterpret_cast<short*>(_ptr);
+    std::printf("d: %06d ", *p2);
+
+    std::cout << std::endl;
+}
+
+void print32(void* _ptr) noexcept
+{
+    unsigned int* p = reinterpret_cast<unsigned int*>(_ptr);
+    std::printf("h: %08X ", *p);
+    std::cout << "b: " << std::bitset<32>(*p) << " ";
+    std::printf("u: %010u ", *p);
+
+    int* p2 = reinterpret_cast<int*>(_ptr);
+    std::printf("d: %010d ", *p2);
+
+    float* p3 = reinterpret_cast<float*>(_ptr);
+    std::printf("f: %5.2f ", *p3);
+    std::printf("fe: %016e ", *p3);
+
+    std::cout << std::endl;   
+}
+
+
+void task1() {
+    short m = 
+}
+
+int main() {
+    std::cout << "hello";
 }
