bf7ece174062c808d6ff812468d92f2b09c97997
reosfire <2.com9761721@gmail.com>
2024-04-06 12:33:46 +0300

    restore changes dimana

--- lab4/main.cpp
+++ lab4/main.cpp
@@ -21,24 +21,24 @@ void movShortByIndex(short* dest, int idx) {
 }
 
 void movIntByIndex(int* dest, int idx) {
-    asm ("movl %1, %0":"=r"(dest[idx]):"r"(16));
+    asm ("movl %1, %0":"=r"(dest[idx]):"i"(16));
 }
 
 void movLongLongByIndex(long long* dest, int idx) {
-    asm ("movq %1, %0":"=r"(dest[idx]):"r"(16ll));
+    asm ("movq %[val], %[dest]":[dest]"=r"(dest[idx]):[val]"r"(16ll));
 }
 
 void movLongLongTask2(long long* arr, size_t idx) {
-    asm ("movq $-1, (%0, %1, 8)"::"r"(arr),"r"(idx));
+    asm ("movq $-1, (%[start], %[offset], 8)"::[start]"r"(arr),[offset]"r"(idx));
 }
 
 void movTask3(long long* arr, size_t idx) {
-    asm ("movb $0xBB, (%0, %1, 1)"::"r"(arr),"r"(idx));
+    asm ("movb $0xBB, (%[start], %[offset], 1)"::[start]"r"(arr),[offset]"r"(idx));
 }
 
 void movTask4(long long* arr, size_t idx, long long* value) {
     long long tmp;
-    asm ("movq (%1), %0":"=r"(tmp):"r"(value));
+    asm ("movq (%[source]), %[dest]":[dest]"=r"(tmp):[source]"r"(value));
     asm ("movq %2, (%0, %1, 8)"::"r"(arr),"r"(idx),"r"(tmp));
 }
 
@@ -50,9 +50,9 @@ void movTask5(long long* arr, size_t idx, long long* value) {
 
 void movTask6(long long& z, long long& w, long long x, long long y) {
     asm ("movq %1, %0":"=r"(z):"r"(x));
-    asm ("addq %1, %0":"+r"(z):"r"(y));
+    asm ("addq %1, %0":"+r"(z):"r"(y):"cc");
     asm ("movq %1, %0":"=r"(w):"r"(x));
-    asm ("subq %1, %0":"+r"(w):"r"(y));
+    asm ("subq %1, %0":"+r"(w):"r"(y):"cc");
 }
 
 void movTask7(double* arr, double x, size_t i) {
@@ -61,13 +61,13 @@ void movTask7(double* arr, double x, size_t i) {
     bool AVX_bit = (C & (1 << 28)) != 0;
     std::cout << "AVX: " << std::boolalpha << AVX_bit << std::endl;
     
-    asm ("vmovsd %0, %%xmm2"::"rq"(x));
-    asm ("vmovsd %xmm2, (%0, %1, 8)":"=rm"(arr):"r"(i));
+    asm ("vmovsd %0, %%xmm2"::"Xm"(x):"%xmm2");
+    asm ("vmovsd %%xmm2, (%0, %1, 8)"::"r"(arr),"X"(i));
 }
 
 void movTask8(double* arr, long long x, size_t i) {
-    asm ("vcvtsi2sd %0, %%xmm1, %%xmm2"::"r"(x));
-    asm ("vmovsd %xmm1, (%0, %1, 8)":"=rm"(arr):"r"(i));
+    asm ("vcvtsi2sd %0, %%xmm1, %%xmm2"::"rm"(x):"%xmm1","%xmm2");
+    asm ("vmovsd %%xmm2, (%0, %1, 8)"::"r"(arr),"rm"(i));
 }
 
 
@@ -161,26 +161,22 @@ int main(int _argc, char* _argv[])
 
     std::cout << "Before:" << std::endl;
     std::cout.precision(10);
-    printArray(Mfs, N, "Mfs");
     printArray(Mfl, N, "Mfl");
 
     movTask7(Mfl, 7.254, 1);
 
     std::cout << "\nAfter:" << std::endl;
-    printArray(Mfs, N, "Mfs");
     printArray(Mfl, N, "Mfl");
 
     std::cout << "\nTASK 8\n" << std::endl;
 
     std::cout << "Before:" << std::endl;
     std::cout.precision(10);
-    printArray(Mfs, N, "Mfs");
     printArray(Mfl, N, "Mfl");
 
-    movTask7(Mfl, 7.254, 1);
+    movTask8(Mfl, 10, 3);
 
     std::cout << "\nAfter:" << std::endl;
-    printArray(Mfs, N, "Mfs");
     printArray(Mfl, N, "Mfl");
 
     return 0;
